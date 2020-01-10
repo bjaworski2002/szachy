1949561afd03b4acdbefca27779e0b26c0035873
aiusz <kuboszek.arkadiusz@gmail.com>
2020-01-10 14:03:55 +0100

    Dodalem plik main.cpp z funkcja dodaj

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int dodaj(int a, int b){
+    return a+b;
+}
+
+
+int main()
+{
+    int a = 5;
+    int b= 10;
+    cout<<  dodaj(a,b);
+    return 0;
+}
