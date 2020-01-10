8a2c04ec2959d0925112d77a39b56da7c222e4ce
bjaworski2002 <bjaworski2002@gmail.com>
2020-01-10 14:10:05 +0100

    Dodalem funkcje pommnoz

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@ int dodaj(int a, int b){
 int odejmij (int a, int b){
     return a-b;
 }
-
+int pomnoz (int a, int b){
+    return a*b;
+}
 int main()
 {
     int a = 5;
     int b= 10;
     cout<<  dodaj(a,b);
-    cout << endl << odejmij (a,b);
+    cout << endl << odejmij (a,b) << " " << pomnoz(a,b);
     return 0;
 }
