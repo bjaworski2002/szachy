9ce2f8e1609ff432b20c6bdb78ddea8e5e69fa2f
bjaworski2002 <bjaworski2002@gmail.com>
2020-01-10 14:08:42 +0100

    Dodalem funkcje odejmij

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int dodaj(int a, int b){
     return a+b;
 }
-
+int odejmij (int a, int b){
+    return a-b;
+}
 
 int main()
 {
     int a = 5;
     int b= 10;
     cout<<  dodaj(a,b);
+    cout << endl << odejmij (a,b);
     return 0;
 }
